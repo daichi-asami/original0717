5306104e39ac697bd28439500823fc5a78361fdc
Daichi Asami <daichi@daichinomacbook-puro.local>
2019-01-23 18:57:46 +0900

    Initial Commit

---
+++ original0717/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  original0717
+//
+//  Created by Daichi Asami on 2019/01/23.
+//  Copyright © 2019年 litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
